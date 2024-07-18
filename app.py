6b701225d3935f7446ffbea14cc10041f00e0e7b
Mathias von Buxhoeveden <mathiasvonbuxhoeveden@Mathiass-MacBook-Pro.local>
2024-07-18 22:54:14 +0200

    small commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from openai import OpenAI
+import os
+import yaml
+
+def main():
+    st.title("Speach to text app")
+    with open('config.yaml', 'r') as file:
+        key_file = yaml.safe_load(file)
+    open_ai_key = key_file['open_ai_key']
+
+    # File uploader widget
+    uploaded_file = st.file_uploader("Choose a file", type=None)
+
+    if uploaded_file is not None:
+        client = OpenAI(api_key = open_ai_key)
+        transcription = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=uploaded_file
+        )
+        text_file = open("transcription.txt", "w")
+        text_file.write(transcription.text)
+        text_file.close()
+
+
+        # Download button
+        st.download_button(
+            label="Download file",
+            data=transcription.text
+
+        )
+
+if __name__ == "__main__":
+    main()
